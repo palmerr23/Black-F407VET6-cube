728c6585bf6283da3ca508e5b85afcb9d3d3e5ec
palmerr23 <palmerr23@gmail.com>
2017-05-09 16:50:05 +1000

    New tests

---
+++ examples/ADC_DualModeInterleaved/ADC_DualModeInterleaved.ino
@@ -0,0 +1,301 @@
+/**
+ * 8 bit samples, ADC1 & 2, DMA
+ * Interrupt processes can eat all the CPU time, so long sample times and big buffers to reduce the number of interrupts.
+ * Example uses software trigger for burst mode sampling. Conversion can be kept running by moving ADC/DMA start to setup().
+ * 
+  ******************************************************************************
+  * @file    ADC/ADC_DualModeInterleaved/Src/main.c 
+  * @author  MCD Application Team
+  * @version V1.4.0
+  * @date    17-February-2017
+  * Modified 6-May-2017 RP for Arduino
+  * @brief   This example provides a short description of how to use the ADC 
+  *          peripheral to convert a regular channel in Dual interleaved mode.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+/** @addtogroup STM32F4xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup ADC_DualModeInterleaved
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* ADC handler declaration */
+ADC_HandleTypeDef    AdcHandle1;
+ADC_HandleTypeDef    AdcHandle2;
+
+/* Variable used to get converted value */
+__IO uint16_t uhADCDualConvertedValue = 0;
+#define NSAMPLES 512
+__IO uint8_t convBuffer[NSAMPLES*4]; // 2 samples per cycle. DMA transfers at 32 bit boundaries, half are empty. 
+
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+static void ADC_Config(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+void setup()
+{
+    SERIALX.begin(9600);
+  delay(200);
+  SERIALX.println("ADC Demo - Dual Mode 8 bit Interleaved Conversion, ADC1 & ADC2, PA1, DMA Mode 3 ");
+  
+  /* STM32F4xx HAL library initialization:
+       - Configure the Flash prefetch, instruction and Data caches
+       - Configure the Systick to generate an interrupt each 1 msec
+       - Set NVIC Group Priority to 4
+       - Global MSP (MCU Support Package) initialization
+     */
+  HAL_Init();
+  
+  /* Configure LED1 and LED3 */
+  BSP_LED_Init(LED1);
+  BSP_LED_Init(LED3);
+  BSP_LED_Off(LED1);
+  BSP_LED_Off(LED3);
+  /*##-1- Configure ADC1 and ADC2 peripherals ################################*/
+  ADC_Config();
+
+  SERIALX.println("Starting ADC2"); 
+    /*##-2- Enable ADC2 ########################################################*/
+  if(HAL_ADC_Start(&AdcHandle2) != HAL_OK)
+  {
+    /* Start Error */
+    Error_Handler(); 
+  }
+  SERIALX.println("Starting ADC1 & DMA"); 
+  /*##-3- Start ADC1 and ADC2 multimode conversion process and enable DMA ####*/
+  /* Note: Considering IT occurring after each number of ADC conversions      */
+  /*       (IT by DMA end of transfer), select sampling time and ADC clock    */
+  /*       with sufficient duration to not create an overhead situation in    */
+  /*        IRQHandler. */
+  if(HAL_ADCEx_MultiModeStart_DMA(&AdcHandle1, (uint32_t*)convBuffer, NSAMPLES) != HAL_OK)
+  {
+    /* Start Error */
+    Error_Handler(); 
+  }
+}
+void loop(void){ 
+ // ADC/DMA can be left running. Stopping it leaves the buffer unchanged while it's being printed.
+ // Interrupt handling can consume significant CPU.
+
+
+ 
+   BSP_LED_Off(LED1); // turn off indicator that DMA is completed
+
+   SERIALX.print("Values = ");
+   for(int i=0; i < min(32, NSAMPLES); i++){ // only print the first few samples to speed things up
+     SERIALX.print(convBuffer[i]);
+     SERIALX.print(", ");
+     if(i % 8 == 7) SERIALX.println("");
+    }
+    SERIALX.println("");
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+/* Short pulses */
+   while(1) { 
+     BSP_LED_On(LED3);
+     delay (100);
+     BSP_LED_Off(LED3);
+     delay (500);
+    } 
+}
+
+/**
+  * @brief  ADC configuration
+  * @note   This function Configure the ADC peripheral  
+            1) Enable peripheral clocks
+            2) Configure ADC Channel 12 pin as analog input
+            3) DMA2_Stream0 channel2 configuration
+            4) Configure ADC1 Channel 12
+            5) Configure ADC2 Channel 12             
+  * @param  None
+  * @retval None
+  */
+static void ADC_Config(void)
+{
+  ADC_ChannelConfTypeDef sConfig;
+  ADC_MultiModeTypeDef   mode;
+  
+  /*##-1- Configure the ADC2 peripheral ######################################*/
+  AdcHandle2.Instance          = ADCy;
+  
+  AdcHandle2.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
+  AdcHandle2.Init.Resolution = ADC_RESOLUTION_8B;
+  AdcHandle2.Init.ScanConvMode = ENABLE;
+  AdcHandle2.Init.ContinuousConvMode = ENABLE;
+  AdcHandle2.Init.DiscontinuousConvMode = DISABLE;
+  AdcHandle2.Init.NbrOfDiscConversion = 0;
+  AdcHandle2.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  AdcHandle2.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
+  AdcHandle2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  AdcHandle2.Init.NbrOfConversion = 1;
+  AdcHandle2.Init.DMAContinuousRequests = ENABLE;
+  AdcHandle2.Init.EOCSelection = ENABLE;
+      
+  if(HAL_ADC_Init(&AdcHandle2) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler(); 
+  }
+  
+  /*##-2- Configure ADC2 regular channel #####################################*/  
+  sConfig.Channel = ADCxy_CHANNEL;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES; //ADC_SAMPLETIME_84CYCLES; //ADC_SAMPLETIME_3CYCLES
+  sConfig.Offset = 0;
+  
+  if(HAL_ADC_ConfigChannel(&AdcHandle2, &sConfig) != HAL_OK)
+  {
+    /* Channel Configuration Error */
+    Error_Handler(); 
+  }
+  
+  /*##-3- Configure the ADC1 peripheral ######################################*/
+  AdcHandle1.Instance          = ADCx;
+  
+  AdcHandle1.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
+  AdcHandle1.Init.Resolution = ADC_RESOLUTION_8B;
+  AdcHandle1.Init.ScanConvMode = DISABLE;
+  AdcHandle1.Init.ContinuousConvMode = ENABLE;
+  AdcHandle1.Init.DiscontinuousConvMode = DISABLE;
+  AdcHandle1.Init.NbrOfDiscConversion = 0;
+  AdcHandle1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  AdcHandle1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
+  AdcHandle1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  AdcHandle1.Init.NbrOfConversion = 1;
+  AdcHandle1.Init.DMAContinuousRequests = ENABLE;
+  AdcHandle1.Init.EOCSelection = ENABLE;
+      
+  if(HAL_ADC_Init(&AdcHandle1) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler(); 
+  }
+  
+  /*##-4- Configure ADC1 regular channel #####################################*/  
+  if(HAL_ADC_ConfigChannel(&AdcHandle1, &sConfig) != HAL_OK)
+  {
+    /* Channel Configuration Error */
+    Error_Handler(); 
+  }
+  
+  /*##-5- Configure Multimode ################################################*/
+  mode.Mode = ADC_DUALMODE_INTERL;
+  mode.DMAAccessMode = ADC_DMAACCESSMODE_3;
+  mode.TwoSamplingDelay = ADC_TWOSAMPLINGDELAY_6CYCLES; 
+  if(HAL_ADCEx_MultiModeConfigChannel(&AdcHandle1, &mode) != HAL_OK)
+  {
+    /* Channel Configuration Error */
+    Error_Handler(); 
+  }
+}
+ 
+/**
+  * @brief  Conversion complete callback in non blocking mode 
+  * @param  hadc : hadc handle
+  * @note   This example shows a simple way to report end of conversion, and 
+  *         you can add your own implementation.    
+  * @retval None
+  */
+  int counter;
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+  /* Turn LED1 on: Transfer process is correct */
+  BSP_LED_On(LED1);
+/*  SERIALX.print("X"); 
+  if (counter++ % 64 == 0)
+    SERIALX.println(" ");
+    */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+
+   SERIALX.print("Assert failed! ");
+   SERIALX.print("File '");
+   SERIALX.print((char*)file);
+   SERIALX.print("', Line ");
+   SERIALX.println(line);
+  
+  /* Rapid blink */
+   while(1) { 
+     BSP_LED_On(LED3);
+     delay (100);
+     BSP_LED_Off(LED3);
+     delay (100);
+    } 
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
